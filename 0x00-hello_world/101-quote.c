a97be6cdcc4803b97f92314c49d0cee5fbb14389
ahmedaymanllll <aa2709123@gmail.com>
2023-08-11 07:14:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: print a qoute useing write function
- * 	ssize_t write(int fd. const void *buf. size_t count);
+ * Description: print a qoute using write function
+ * 	ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: 1 (error).
 */
@@ -13,6 +13,6 @@
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1. quo. 59);
+	write(1, quo, 59);
 	return (1);
 }
