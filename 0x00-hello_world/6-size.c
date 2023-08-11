7fc88b799da8290253a8b9b4a7452902861857fa
ahmedaymanllll <aa2709123@gmail.com>
2023-08-10 19:54:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: using sizeof to print 
+ * Description: using sizeof to print the size of arious types.
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("Size of a char: %1u byte(s)\n",sizeof(char));
-	printf("Size of a int: %1u byte(s)\n",sizeof(int));
-	printf("Size of a long int: %1u byte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %1u byte(s)\n",sizeof(long long int));
-	printf("Size of a float: %1u byte(s)\n",sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
