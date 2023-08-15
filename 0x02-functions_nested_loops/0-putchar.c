64c739ea04fa6669ac748416d21201a8fa9f350f
ahmedaymanllll <aa2709123@gmail.com>
2023-08-15 11:59:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: How to Write a program prints _putchar, followed by new line.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
