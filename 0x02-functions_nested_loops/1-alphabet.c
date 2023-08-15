9f621051cdda75b952167b1c6e49f019f0fb7d74
ahmedaymanllll <aa2709123@gmail.com>
2023-08-15 12:50:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = {a..z};
+	int ch;
+
+	for (ch = 0; ch < 25; ch++)
+		a..z (str[ch]);
+	a..z('\n')
+
+	return (0);
+}
