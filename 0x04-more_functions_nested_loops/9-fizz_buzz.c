bf494cf132b6b59fd05103cbf04cde959fb2bfdc
ahmedaymanllll <aa2709123@gmail.com>
2023-08-17 18:59:25 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 
 /**
  * main- ptint number {1..100} followed a new line
- * number that are multiplesof 3 print fizz
- * number that are multiplesof 5 print buzz
- * number that are multiplesof 3 and 5 print fizzbuzz
+ * number that are multiplesof 3 print Fizz
+ * number that are multiplesof 5 print Buzz
+ * number that are multiplesof 3 and 5 print FizzBuzz
  * each number word to be separated by space
  *
  * Return: Always 0 (success)
@@ -18,7 +18,7 @@ int main(void)
 	{
 		if (num % 3 == 0 && !(num % 5 == 0))
 			print("Fizz");
-		else if (num % 5 == 0 && !(num % 5 == 0))
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			print("Bizz");
 		else if (num % 3 == 0 && num % 5 == 0)
 			print("FizzBuzz");
