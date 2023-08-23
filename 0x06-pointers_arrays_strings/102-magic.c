89eb7d6783e8d8fe15a264c78f4750230fd83b36
ahmedaymanllll <aa2709123@gmail.com>
2023-08-23 16:43:02 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point a program
+ * Description: A dream doesn't become reality through magic
+ * Return: zero
+*/
+int main(void)
+{
+	int n, a[5], *p;
+
+	a[2] = 1024;
+	p = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+*/
+	*(p + 5) = 98;
+/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
