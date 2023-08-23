33440e0740ec226365eb57acd9e38ce760a6d7b1
ahmedaymanllll <aa2709123@gmail.com>
2023-08-23 11:27:17 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strncat - Write a function that concatenates two strings.
+ *
+ * @dest: the frist input
+ * @src: the secound input
+ * @n: the there input
+ *
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, int b;
+
+	a = 0;
+	if (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	if else(b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
