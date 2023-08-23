cbab25e1f4d44aa3f341398bc465a1b76ac7fa4a
ahmedaymanllll <aa2709123@gmail.com>
2023-08-23 10:38:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,14 +16,17 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 
-	while (dest[i])
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	i2 = 0;
+	while (src[i2] != '\0')
 	{
-	for (i2 = 0; src[i2]; i2++)
-
-		dest[i++] = src[i2++];
+		i++;
+		i2++;
 	}
+
+		dest[i] = '\0';
 	return (dest);
 }
