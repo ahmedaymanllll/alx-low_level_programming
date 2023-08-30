6c5398eda0e5d0c95e8054a88a069818632ef460
ahmedaymanllll <aa2709123@gmail.com>
2023-08-29 18:20:16 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Write a function that searches a string
+ * for any of a set of bytes
+ *
+ * @s: input
+ * @accept: input
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			return (s);
+		}
+	s++;
+	}
+	return ('\0')
+}
