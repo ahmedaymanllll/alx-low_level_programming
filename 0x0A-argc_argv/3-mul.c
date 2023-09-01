9feb47815f84d14403c84e84f54c2b89cd574553
ahmedaymanllll <aa2709123@gmail.com>
2023-09-01 15:57:15 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that multiplies two numbers
+ *
+ * @argc: number
+ * @argv: pointer arry
+ *
+ * Return: zero success, no zero-not  success
+*/
+
+int main(int argc, char *argv[])
+{
+	int num;
+
+	if (argc == 3)
+	{
+		num =  atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", num);
+	}
+	else
+	{
+		printf("erro\n");
+		return (1);
+	}
+	return (0);
+}
