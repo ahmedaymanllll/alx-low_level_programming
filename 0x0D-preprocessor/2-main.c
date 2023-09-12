db35498f96a949a5974f48ce4cc3b6155c333251
ahmedaymanllll <aa2709123@gmail.com>
2023-09-11 21:08:51 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <sttdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Always (0).
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
