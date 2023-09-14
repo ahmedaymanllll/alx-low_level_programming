ed513e994bdeacd0db9ec2083b8b3c97c7d235d7
ahmedaymanllll <aa2709123@gmail.com>
2023-09-13 20:43:14 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two integers
+ * @a: frist int
+ * @b: second int
+ *
+ * Return: sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: fris int
+ * @b: second int
+ *
+ * Return: difference
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integer
+ * @a: the frist int
+ * @b: the second int
+ *
+ * Return: the product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the frist int
+ * @b: the second int
+ *
+ * Return: qoutient
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo two integers
+ * @a: the frist int
+ * @b the frist int
+ *
+ * Return: remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
