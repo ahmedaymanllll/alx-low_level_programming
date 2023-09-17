3d3afcd3250600a347c92a774772246a676407d9
ahmedaymanllll <aa2709123@gmail.com>
2023-09-17 00:21:57 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,13 +30,13 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = "\0";
+		s1 = '\0';
 	if (s2 == NULL)
-		s2 == "\0";
+		s2 == '\0';
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
-	m = malloc((size1 + size2) *sizeof(char) + a);
+	m = malloc((size1 + size2) * sizeof(char) + a);
 	if (m == 0)
 		return (0);
 
