112edb3c02dba03ff80f6b0857999e3e26109116
ahmedaymanllll <139121821+ahmedaymanllll@users.noreply.github.com>
2023-09-26 18:35:28 +0300

    Add files via upload

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function to print the alphabet a - z in 10 lines.
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
+
